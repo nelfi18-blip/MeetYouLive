30bc509062c21fca2a0da70b894db6948a4375e5
nelfi18-blip <nelfi18@gmail.com>
2026-03-07 23:16:30 -0500

    Refactor NextAuth route.js for cleaner syntax

--- frontend/app/api/auth/[...nextauth]/route.js
+++ frontend/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 const handler = NextAuth({
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -21,31 +22,39 @@ const handler = NextAuth({
   },
 
   callbacks: {
+
+    async signIn() {
+      return true
+    },
+
     async jwt({ token, account, profile }) {
+
       if (account) {
-        token.accessToken = account.access_token;
+        token.accessToken = account.access_token
       }
 
       if (profile) {
-        token.name = profile.name;
-        token.email = profile.email;
-        token.picture = profile.picture;
+        token.name = profile.name
+        token.email = profile.email
+        token.picture = profile.picture
       }
 
-      return token;
+      return token
     },
 
     async session({ session, token }) {
+
       if (token) {
-        session.user.name = token.name;
-        session.user.email = token.email;
-        session.user.image = token.picture;
-        session.accessToken = token.accessToken;
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
       }
 
-      return session;
+      return session
     },
-  },
-});
 
-export { handler as GET, handler as POST };
+  },
+
+})
+
+export { handler as GET, handler as POST }
